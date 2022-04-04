f36d5c477abe2aa4a09f55126c7efc14466646bc
Ousmane Doudou Ridouane <ridousmane9845@gmail.com>
2022-04-05 00:25:38 +0200

    ok

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,8 +16,17 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('prenom');
             $table->string('email')->unique();
             $table->string('fonction');
+            $table->string('num_reference');
+            $table->date('dateNaiss');
+            $table->string('pays');
+            $table->string('ville');
+            $table->string('codePostal');
+            $table->string('numTel');
+            $table->string('sexe');
+            $table->string('image');
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
