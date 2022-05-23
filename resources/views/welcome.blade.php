aed6eb55cbb36733e573f4b7d8e2bacc94950116
Ousmane Doudou Ridouane <ridousmane9845@gmail.com>
2022-05-23 14:19:58 +0100

    m

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -53,7 +53,7 @@
 
 </head>
 
-<body>
+<body onload = "getLocation();">
 
     <div class="site-wrap">
 
@@ -69,6 +69,7 @@
                     </form>
                 </div>
             </div>
+
 
             <div class="container">
                 <div class="d-flex align-items-center justify-content-between">
@@ -292,19 +293,18 @@
       <div id="resultPharmacie" style="display: none">
         <div class="row staff-grid-row">
                 @foreach ($pharmacies as $lists )
-                    <div class="col-md-4 col-sm-6 col-12 col-lg-4 col-xl-3">
-                        <div class="profile-widget">
-                            <div class="text-center">
-                            <a href="{{URL::to('/')}}" class="brand-link"> <img class="img-fluid" src="{{ URL::to('storage/files/'. $lists->pharmacie_image) }}" alt="User profile picture" style="max-width:200px;margin-bottom:10px;"></a>
-                            <h4 class="user-name m-t-10 mb-0 text-ellipsis"><a href="">Nom: {{ $lists->name }}</a></h4>
-                            <h5 class="user-name m-t-10 mb-0 text-ellipsis">Localite: {{ $lists->localite }}</h5>
-                            <div class="small text-muted">NbrA: {{ $lists->nbrAgent }}</div>
-                    </tbody>
-                @endforeach
+                <div class="col-md-4 col-sm-6 col-12 col-lg-4 col-xl-3">
+                    <div class="profile-widget">
+                        <div class="text-center">
+                        <a href="{{URL::to('/')}}" class="brand-link"> <img class="img-fluid" src="{{ URL::to('storage/files/'. $lists->pharmacie_image) }}" alt="User profile picture" style="max-width:200px;margin-bottom:10px;"></a>
+                          <h4 class="user-name m-t-10 mb-0 text-ellipsis"><a href="">Nom: {{ $lists->name }}</a></h4>
+                          <h5 class="user-name m-t-10 mb-0 text-ellipsis">Localite: {{ $lists->localite }}</h5>
+                        <div class="small text-muted">NbrA: {{ $lists->nbrAgent }}</div>
+                </tbody>
             </table>
         </div>
 
-        
+        {{-- </div> --}}
 
         <footer class="site-footer bg-light">
             <div class="container">
@@ -396,5 +396,5 @@
 
 
 </body>
-
 </html>
+ 
